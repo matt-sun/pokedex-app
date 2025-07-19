5da0bde5996c7c86af883365f9671e44db61ced3
Matthieu Sun <matthieusun@Matthieus-MacBook-Pro.local>
2025-07-19 23:11:53 +0200

    feat(pages): Add Home, Favorites, About and Detail pages.

---
+++ src/pages/Detail.tsx
@@ -0,0 +1,10 @@
+function Detail() {
+  return (
+    <div>
+      <h1>Detail</h1>
+      <p>This is the Detail page.</p>
+    </div>
+  );
+}
+
+export default Detail;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import PokemonGrid from "@/components/pokemon/PokemonGrid";
+
+function Home() {
+  return (
+    <div>
+      <PokemonGrid />
+    </div>
+  );
+}
+
+export default Home;
