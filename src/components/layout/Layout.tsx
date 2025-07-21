269c2abecc1087a012c68bb3303d429aa2c4ad09
Matthieu Sun <matthieusun@Matthieus-MacBook-Pro.local>
2025-07-21 18:14:26 +0200

    update(Layout.tsx): move ThemeProvider to App.tsx for consistency

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import { ThemeProvider } from "@/components/layout/ThemeProvider";
 
 function Layout() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="font-pokemon flex min-h-screen flex-col bg-gray-200">
-        <Header />
-        <main className="grow mx-4">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <div className="font-pokemon flex min-h-screen flex-col bg-gray-200">
+      <Header />
+      <main className="grow mx-4">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
