d6b71b5144d1bd0b7e800b89c0e57b9617fff5db
Matthieu Sun <matthieusun@Matthieus-MacBook-Pro.local>
2025-07-24 17:20:17 +0200

    update(Home.tsx): move Paginate component to Home.tsx for consistency

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import PokemonGrid from "@/components/pokemon/PokemonGrid";
+import Paginate from "@/components/layout/Paginator";
 
 function Home() {
   return (
     <div>
       <PokemonGrid />
+      <Paginate />
     </div>
   );
 }
