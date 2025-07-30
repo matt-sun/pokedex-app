3eba07465ec9da85c867add13a5b1cff7b634d7e
Matthieu Sun <matthieusun@Matthieus-MacBook-Pro.local>
2025-07-30 20:45:50 +0200

    update(types.ts): update types to match desired output and add required API typing

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,12 @@
+// Custom Types
+
 interface Pokemon {
-  id: number;
+  id?: number;
   name: string;
   url: string;
-  spriteFront: string;
-  spriteShiny?: string;
-  type1: string;
-  type2?: string;
-  stats: {
+  sprites?: { front: string; shiny: string };
+  types?: { 1: string; 2: string };
+  stats?: {
     hp: number;
     attack: number;
     defense: number;
@@ -14,18 +14,173 @@ interface Pokemon {
     speDef: number;
     speed: number;
   };
+  abilities?: PokemonAbilities[];
+  evolutions?: PokemonEvolutions[];
+  locations?: PokemonLocations[];
+  moves?: PokemonMoves[];
+  // favorite: boolean;
 }
 
-//   ability: string; //Expand to show desc
-//   abilitiyDesc: string;
-//   move: string;
-//   movePwr: number;
-//   moveAcc: number;
-//   moveType: string;
-//   species: string; // We need to get the species from this endpoint: https://pokeapi.co/api/v2/pokemon-species/{id or name}/
-//   // Then we can get the evolution chain url from which we can extract the id https://pokeapi.co/api/v2/evolution-chain/2/
-//   evolution: string; // We use the id from right above to get the Evolution Chain.
-//   // Read doc to figure out how to get the ID the pokemon part of the evolution Chain
-//   location: string; // If applicable. This requires a specific endpoint https://pokeapi.co/api/v2/pokemon/{id or name}/encounters
+interface PokemonAbilities {
+  name: string;
+  hidden?: boolean;
+  effect?: string;
+  shortened?: string;
+}
 
-export type { Pokemon };
+interface PokemonEvolutions {
+  name: string;
+  sprite: string;
+}
+
+interface PokemonLocations {
+  name: string;
+}
+
+interface PokemonMoves {
+  name: string;
+  accuracy: number;
+  power: number;
+  pp: number;
+  type: string;
+}
+
+// Pokedex API Responses
+
+interface AllPokemonAPI {
+  name: string;
+  url: string;
+}
+
+interface AbilitiesAPI {
+  is_hidden: boolean;
+  ability: {
+    name: string;
+    url: string;
+  };
+}
+
+interface AbilityAPI {
+  effect_entries: EffectAPI[];
+}
+
+interface EffectAPI {
+  effect?: string;
+  language?: {
+    name: string;
+  };
+  short_effect?: string;
+}
+
+interface LocationAreasAPI {
+  results: LocationAreaAPI[];
+}
+
+interface LocationAreaAPI {
+  location_area: {
+    url: string;
+  };
+}
+//CHECK AGAIN
+interface LocationAPI {
+  names: {
+    language: {
+      name: string;
+    };
+    name: string;
+  };
+}
+
+interface MovesAPI {
+  move: { name: string; url: string };
+}
+
+interface MoveAPI {
+  accuracy: number;
+  power: number;
+  pp: number;
+  type: {
+    name: string;
+  };
+}
+
+interface SpeciesAPI {
+  evolution_chain: {
+    url: string;
+  };
+}
+
+interface EvolutionChainAPI {
+  baby_trigger_item: string;
+  chain: {
+    species: {
+      name: string;
+      url: string;
+    };
+    evolves_to: ChainLinkAPI[];
+  };
+}
+
+interface ChainLinkAPI {
+  species: {
+    name: string;
+    url: string;
+  };
+  evolves_to: ChainLinkAPI[];
+}
+
+interface StatsAPI {
+  base_stat: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+interface TypesAPI {
+  type: { name: string; url: string };
+}
+
+interface PokemonAPI {
+  id: number;
+  abilities: AbilitiesAPI[];
+  location_area_encounters: string;
+  moves: MovesAPI[];
+  species: {
+    name: string;
+    url: string;
+  };
+  sprites: {
+    other: {
+      ["official-artwork"]: {
+        front_default: string;
+        front_shiny: string;
+      };
+    };
+  };
+  stats: StatsAPI[];
+  types: TypesAPI[];
+}
+
+export type {
+  Pokemon,
+  PokemonAbilities,
+  PokemonEvolutions,
+  PokemonLocations,
+  PokemonMoves,
+  AllPokemonAPI,
+  AbilitiesAPI,
+  AbilityAPI,
+  EffectAPI,
+  LocationAreasAPI,
+  LocationAreaAPI,
+  LocationAPI,
+  MovesAPI,
+  MoveAPI,
+  SpeciesAPI,
+  EvolutionChainAPI,
+  ChainLinkAPI,
+  StatsAPI,
+  TypesAPI,
+  PokemonAPI,
+};
