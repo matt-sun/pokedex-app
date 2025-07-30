f8e2a327dbd5dcc756bf433de381ce89d0a53f0d
Matthieu Sun <matthieusun@Matthieus-MacBook-Pro.local>
2025-07-30 21:16:58 +0200

    update(components/pokemon): update data loading process
    add loading and error handling.
    
    use useContext to get data from App.tsx context and leverage cached data if exist.
    
    add useParams in Pokemon.tsx to use URL name parameter to get correct Pokemon data.

--- src/components/pokemon/PokemonGrid.tsx
+++ src/components/pokemon/PokemonGrid.tsx
@@ -1,28 +1,36 @@
+import { useContext } from "react";
 import PokemonCard from "@/components/pokemon/PokemonCard";
-import API from "@/hooks/usePokemon";
 import { Link } from "react-router-dom";
+import { PokemonContext } from "@/App";
+import type { Pokemon } from "@/lib/types";
 
 function PokemonGrid() {
-  const pokemons = API();
+  const { pokemons, isLoading, error } = useContext(PokemonContext);
+
+  // console.log("PokemonGrid render:", { pokemons, isLoading, error });
+
+  if (error) {
+    return <div className="text-center p-4">Error: {error}</div>;
+  }
+
+  if (isLoading) {
+    return <div className="text-center p-4">Loading...</div>;
+  }
 
   return (
     <div className="grid grid-cols-4 mx-auto w-8/10 max-w-5xl min-w-2xs rounded-2xl gap-4 justify-items-center">
-      {pokemons.map((pokemon) => {
-        return (
-          <Link to={`pokemon/${pokemon.name}`}>
-            <PokemonCard
-              key={pokemon.id}
-              id={pokemon.id}
-              name={pokemon.name}
-              spriteF={pokemon.spriteFront}
-              spriteS={pokemon?.spriteShiny}
-              type1={pokemon.type1}
-              type2={pokemon?.type2}
-              {...pokemons}
-            />
-          </Link>
-        );
-      })}
+      {pokemons.sort().map((pokemon: Pokemon) => (
+        <Link key={pokemon.id} to={`pokemon/${pokemon.name}`}>
+          <PokemonCard
+            id={pokemon.id}
+            name={pokemon.name}
+            spriteF={pokemon.sprites?.front}
+            spriteS={pokemon.sprites?.shiny}
+            type1={pokemon.types?.[1]}
+            type2={pokemon.types?.[2]}
+          />
+        </Link>
+      ))}
     </div>
   );
 }
--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,60 +1,71 @@
-import { useLoaderData } from "react-router-dom";
-import type { LoaderFunctionArgs } from "react-router-dom";
 import TypeBadge from "@/components/pokemon/TypeBadge";
 import FavoriteButton from "@/components/layout/FavoriteButton";
 import StatChart from "@/components/pokemon/StatChart";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { PokemonContext } from "@/App";
 
-export async function Loader({ params }: LoaderFunctionArgs) {
-  // Fetch the pokemon data by name from params from the PokeAPI
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${params.name}`
-  );
-  if (!response.ok) {
-    throw new Response("Not Found", { status: 404 });
-  }
-  const pokemon = await response.json();
-
-  return { pokemon };
-}
+// Upon reload, we have an error because the data is not cached.
+// To fix this, either I reload the whole API or I cache the data.
+// We will do the latter via IndexedDB
 
 function Pokemon() {
-  const { pokemon } = useLoaderData();
-  console.log(pokemon);
+  const { name } = useParams();
+  const { pokemons, isLoading, error } = useContext(PokemonContext);
 
-  return (
-    <div className="grid grid-cols-4 grid-rows-[auto_1fr_1fr_auto] mx-auto w-6/10 max-w-5xl min-w-2xs gap-y-6 gap-x-4 justify-items-center">
-      <div className="col-span-4 grid grid-cols-subgrid gap-4 content-start">
-        <div className="capitalize col-span-2 justify-self-end">
-          {pokemon.name}
-        </div>
-        <div className="justify-self-start">
-          # {pokemon.id.toString().padStart(4, "0")}
-        </div>
-        <div className="justify-self-end self-end">
-          <FavoriteButton />
-        </div>
-      </div>
-      <img
-        src={pokemon.sprites.other["official-artwork"].front_default}
-        alt={pokemon.name}
-        className="w-100 h-auto col-span-2 row-span-2 justify-self-end self-center"
-      />
-      <div className="col-start-1 col-end-3 row-start-4 row-end-5 justify-self-start self-center h-auto w-full">
-        <StatChart
-          hp={pokemon.stats[0].base_stat}
-          attack={pokemon.stats[1].base_stat}
-          defense={pokemon.stats[2].base_stat}
-          speAtt={pokemon.stats[3].base_stat}
-          speDef={pokemon.stats[4].base_stat}
-          speed={pokemon.stats[5].base_stat}
-        />
-      </div>
-      <div className="flex w-full flex-wrap gap-2 col-span-2 justify-self-center">
-        <TypeBadge type={pokemon.types[0].type.name} />
-        <TypeBadge type={pokemon.types[1]?.type.name} />
-      </div>
-    </div>
+  if (error) {
+    return <div className="text-center p-4">Error: {error}</div>;
+  }
+
+  if (isLoading) {
+    return <div className="text-center p-4">Loading...</div>;
+  }
+
+  const pokemon = pokemons.find(
+    (item) => item.name.toLowerCase() === name?.toLowerCase()
   );
+
+  if (pokemon) {
+    return (
+      <div className="grid grid-cols-4 grid-rows-[auto_1fr_1fr_auto] mx-auto w-6/10 max-w-5xl min-w-2xs gap-y-6 gap-x-4 justify-items-center">
+        <div className="col-span-4 grid grid-cols-subgrid gap-4 content-start">
+          <div className="capitalize col-span-2 justify-self-end">
+            {pokemon.name}
+          </div>
+          <div className="justify-self-start">
+            # {pokemon.id?.toString().padStart(4, "0")}
+          </div>
+          <div className="justify-self-end self-end">
+            <FavoriteButton />
+          </div>
+        </div>
+        <img
+          src={pokemon.sprites?.front}
+          alt={pokemon.name}
+          className="w-100 h-auto col-span-2 row-span-2 justify-self-end self-center"
+        />
+        <div className="col-start-1 col-end-3 row-start-4 row-end-5 justify-self-start self-center h-auto w-full">
+          <StatChart
+            hp={pokemon.stats?.hp}
+            attack={pokemon.stats?.attack}
+            defense={pokemon.stats?.defense}
+            speAtt={pokemon.stats?.speAtt}
+            speDef={pokemon.stats?.speDef}
+            speed={pokemon.stats?.speed}
+          />
+        </div>
+        <div className="flex w-full flex-wrap gap-2 col-span-2 justify-self-center">
+          <TypeBadge type={pokemon.types?.[1]} />
+          <TypeBadge type={pokemon.types?.[2]} />
+        </div>
+        {/* Why do I call it as under the PokeAPI format and not the Pokemon object type format I defined? */}
+        <div>{pokemon.abilities?.[0]?.name}</div>
+        <div>{pokemon.abilities?.[0]?.hidden}</div>
+        <div>{pokemon.abilities?.[0]?.effect}</div>
+        <div>{pokemon.abilities?.[0]?.shortened}</div>
+      </div>
+    );
+  }
 }
 
 export default Pokemon;
