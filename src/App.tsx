3e96e250eeb0d4f7c9cddf8f57c876c6b84b25ad
Matthieu Sun <matthieusun@Matthieus-MacBook-Pro.local>
2025-07-30 21:00:04 +0200

    update(App.tsx): create context to pass API data through the lower components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { ThemeProvider } from "@/components/layout/ThemeProvider";
 import Layout from "@/components/layout/Layout";
+import type { Pokemon } from "@/lib/types";
+import { createContext } from "react";
+import usePokemonData from "@/hooks/usePokemon";
+
+interface PokemonContextType {
+  pokemons: Pokemon[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const PokemonContext = createContext<PokemonContextType>({
+  pokemons: [],
+  isLoading: true,
+  error: null,
+});
 
 function App() {
+  const result = usePokemonData();
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div>
-        <Layout />
-      </div>
+      <PokemonContext.Provider value={result}>
+        <div>
+          <Layout />
+        </div>
+      </PokemonContext.Provider>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+export { PokemonContext };
