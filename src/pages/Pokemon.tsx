3d4aec3c2b0792e2632c932f27aa2b0e148df80d
Matthieu Sun <matthieusun@Matthieus-MacBook-Pro.local>
2025-07-31 03:02:01 +0200

    feat(Pokemon.tsx): add Encounter Locations section with accordion style component

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -7,7 +7,9 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { useContext } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { useContext, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import { PokemonContext } from "@/App";
 
@@ -34,12 +36,12 @@ function Pokemon() {
     return (
       <div className="grid grid-cols-4 grid-rows-[auto_auto_1fr_auto_auto_auto] mx-auto w-6/10 max-w-5xl min-w-2xs gap-y-4 gap-x-4 justify-items-center">
         <div className="col-span-4 row-span-2 grid grid-cols-subgrid grid-rows-subgrid gap-x-4 gap-y-0 content-start">
-          <div className="capitalize col-start-2 row-start-1 justify-self-end">
+          <h2 className="capitalize col-start-2 row-start-1 justify-self-end">
             {pokemon.name}
-          </div>
-          <div className="col-start-3 row-start-1 justify-self-start">
+          </h2>
+          <h2 className="col-start-3 row-start-1 justify-self-start">
             # {pokemon.id?.toString().padStart(4, "0")}
-          </div>
+          </h2>
           <div className="col-start-4 row-start-1 justify-self-end self-end">
             <FavoriteButton />
           </div>
@@ -68,7 +70,7 @@ function Pokemon() {
 
         <div className="col-start-1 col-span-4 row-start-4 row-span-1 justify-self-start h-auto w-full border rounded-xl shadow-sm">
           <Accordion type="multiple" className="mx-4">
-            <div className="mt-6 mb-2">Abilities</div>
+            <h3 className="mt-6 mb-2">Abilities</h3>
             {pokemon.abilities?.map((ability, index) => (
               <AccordionItem value={`item-${index}`} key={index}>
                 <AccordionTrigger className="capitalize">
@@ -79,6 +81,48 @@ function Pokemon() {
             ))}
           </Accordion>
         </div>
+
+        <div className="col-start-1 col-span-4 row-start-5 row-span-1 justify-self-start h-auto w-full border rounded-xl shadow-sm">
+          <Accordion type="multiple" className="mx-4">
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="capitalize text-base mt-2">
+                Encounter Locations
+              </AccordionTrigger>
+              <AccordionContent>
+                {pokemon.locations && pokemon.locations.length > 0 ? (
+                  pokemon.locations.map((location, index) => (
+                    <div className="text-sm capitalize mt-3" key={index}>
+                      {location.name}
+                    </div>
+                  ))
+                ) : (
+                  <div className="text-sm capitalize mt-3">Unknown</div>
+                )}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+
+        {/*         
+        <ScrollArea className="col-start-1 col-span-4 row-start-5 row-span-1 justify-self-start h-32 w-full border rounded-xl shadow-sm">
+          <div className="mx-4">
+            <h3 className="my-6">Encounter Locations</h3>
+            {pokemon.locations && pokemon.locations.length > 0 ? (
+              pokemon.locations.map((location, index) => (
+                <Fragment key={index}>
+                  {" "}
+                  <Separator className="my-4 bg-black" />
+                  <div className="text-sm capitalize mb-4">{location.name}</div>
+                </Fragment>
+              ))
+            ) : (
+              <Fragment>
+                <Separator className="my-4 bg-black" />
+                <div className="text-sm capitalize mb-4">Unknown Location</div>
+              </Fragment>
+            )}
+          </div>
+        </ScrollArea> */}
       </div>
     );
   }
