9d1d933157dc74c3114cca9f69faaec865fd5d79
Matthieu Sun <sun.matthieu@gmail.com>
2025-08-13 16:39:44 +0200

    update(ModeToggle.tsx): add cursor-pointer upon hover on button

--- src/components/layout/ModeToggle.tsx
+++ src/components/layout/ModeToggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
         <Button
           variant="ghost"
           size="icon"
-          className="hover:bg-pokemon-boston-red dark:hover:bg-pokemon-gold"
+          className="cursor-pointer hover:bg-pokemon-boston-red dark:hover:bg-pokemon-gold"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
