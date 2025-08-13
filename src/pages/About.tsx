b4a8eb3d37fa7fa0d65658ddd78795c98cfa46ec
Matthieu Sun <sun.matthieu@gmail.com>
2025-08-13 16:46:41 +0200

    update(pages): update display properties to fit new layout
    add new Paginate component to Homepage

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 function About() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the About page.</p>
+    <div className="flex flex-col flex-1">
+      <h1 className="text-3xl text-center my-4 py-4">About</h1>
+      <div className="flex-1">
+        <p className="text-center">This is the About page.</p>
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import PokemonGrid from "@/components/pokemon/PokemonGrid";
-import Paginate from "@/components/layout/Paginator";
+import Paginate from "@/components/layout/Paginate";
 
 function Home() {
+  const totalPokemon = 1025; // Total number of Pokemon
+
   return (
-    <div>
-      <PokemonGrid />
-      <Paginate />
+    <div className="flex flex-col flex-1">
+      <h1 className="text-3xl text-center my-4 py-4">Pokédex</h1>
+      <div className="flex-1">
+        <PokemonGrid />
+      </div>
+      <Paginate totalPokemon={totalPokemon} />
     </div>
   );
 }
