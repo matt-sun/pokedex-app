c531734c0718e3d220bda5e25840655f096121e2
Matthieu Sun <sun.matthieu@gmail.com>
2025-08-16 17:45:29 +0200

    style(PokemonCard.tsx): add padding and CardFooter variation for TypeBadge display under 640px

--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -10,6 +10,7 @@ import {
 import TypeBadge from "@/components/pokemon/TypeBadge";
 import FavoriteButton from "@/components/pokemon/FavoriteButton";
 import type { Pokemon } from "@/lib/types";
+import { useMediaQuery } from "@/lib/use-media-query";
 
 interface Props {
   id: number;
@@ -34,10 +35,12 @@ function PokemonCard(props: Props) {
     },
   };
 
+  const isBreakPointSmall = useMediaQuery("(min-width: 640px)");
+
   return (
     <div className="flex-1">
-      <Card className="w-auto h-full flex flex-col flex-1 ">
-        <CardHeader>
+      <Card className="w-auto h-full flex flex-col flex-1">
+        <CardHeader className="text-sm px-4">
           <CardTitle className="capitalize">{props.name}</CardTitle>
           <CardDescription>
             #{props.id.toString().padStart(5, "0")}
@@ -49,13 +52,24 @@ function PokemonCard(props: Props) {
             />
           </CardAction>
         </CardHeader>
-        <CardContent className="flex-1 flex items-center">
+        <CardContent className="flex-1 flex items-center px-4">
           <img src={props.spriteF} alt={props.name} className="w-full h-auto" />
         </CardContent>
-        <CardFooter className="flex flex-1 w-full items-center gap-2">
-          <TypeBadge type={props.type1} />
-          <TypeBadge type={props.type2} />
-        </CardFooter>
+        {isBreakPointSmall ? (
+          <CardFooter className="flex flex-1 w-full items-center gap-2 px-4">
+            <TypeBadge type={props.type1} />
+            <TypeBadge type={props.type2} />
+          </CardFooter>
+        ) : (
+          <CardFooter className="grid grid-cols-2 grid-rows-1 w-full items-center gap-4 px-4">
+            <div className="justify-self-end">
+              <TypeBadge type={props.type1} />
+            </div>
+            <div className="justify-self-start">
+              <TypeBadge type={props.type2} />
+            </div>
+          </CardFooter>
+        )}
       </Card>
     </div>
   );
