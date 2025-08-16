e5237d1c399d0ef65c228a89f1dbe5f9d1c91e62
Matthieu Sun <sun.matthieu@gmail.com>
2025-08-16 17:49:47 +0200

    style(progress.tsx): add display, opacity and size variations at 640 and 480px for better small screen size fit

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -3,24 +3,37 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+import { useMediaQuery } from "@/lib/use-media-query";
+
 function Progress({
   className,
   value,
   ...props
 }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+  const isBreakPointSmall = useMediaQuery("(min-width: 640px)");
+  const isBreakPointExtraSmall = useMediaQuery("(min-width: 480px)");
+
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "bg-pokemon-blue relative h-2 w-full overflow-hidden rounded-full",
+        "bg-pokemon-blue xs:max-sm:bg-pokemon-blue/70 relative h-2 w-full xs:max-sm:h-full xs:max-sm:w-4 overflow-hidden rounded-full",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-pink-400 h-full w-full flex-1 transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+        className={`bg-pink-400 xs:max-sm:bg-pink-400/70 h-full w-full flex-1 transition-all`}
+        style={{
+          transform: `${
+            isBreakPointSmall
+              ? `translateX(-${100 - (value || 0)}%)`
+              : isBreakPointExtraSmall
+              ? `translateY(-${100 - (value || 0)}%)`
+              : `translateX(-${100 - (value || 0)}%)`
+          }`,
+        }}
       />
     </ProgressPrimitive.Root>
   );
